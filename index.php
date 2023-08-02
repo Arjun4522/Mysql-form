11657b28b574b92890e44f2b7dc5ea4b3ac3e532
Arjun Ghoshal <94633408+Arjun4522@users.noreply.github.com>
2023-08-03 01:06:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Web Version for MySQL</title>
+    <title>Basic LAMP stack</title>
 </head>
 <body>
-    <h1>Add Data</h1>
+    <h1>Enter Data</h1>
     <form action="insert.php" method="post">
         <label for="name">Name:</label>
         <input type="text" name="name" required>
+        <br>
         <label for="email">Email:</label>
         <input type="email" name="email" required>
+        <br>
+        <label for="number">Phone:</label>
+        <input type="text" name="number" required>
+        <br>
+        <br>
         <input type="submit" value="Submit">
     </form>
 
